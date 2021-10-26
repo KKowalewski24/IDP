7224d3863af2e9f43273ac85d3366371d5d24b10
Jan Karwowski <karwojan@tlen.pl>
2021-10-26 23:45:25 +0200

    Fix and finish kohonen implementation

--- task5/kohonen.py
+++ task5/kohonen.py
@@ -2,40 +2,76 @@ import numpy as np
 
 
 class KohonenNetwork():
-    def __init__(self, n_inputs, n_outputs):
-        self.n_inputs = n_inputs
-        self.n_outputs = n_outputs
-        self.W = np.random.rand(n_outputs, n_inputs) * 2 - 1
+    def __init__(self, n_neurons: int, X: np.ndarray, normalize: bool = False):
+        self.n_outputs = n_neurons
+        self.n_inputs = X.shape[1]
+        self.random_generator = np.random.default_rng()
+        self.X = X
+        self.W = self.random_generator.uniform(np.min(self.X),
+                                               np.max(self.X),
+                                               size=(self.n_outputs,
+                                                     self.n_inputs))
+        self.normalize = normalize
+        if normalize:
+            self.X = X / np.sqrt(np.sum(X**2, axis=1, keepdims=True))
+            self.W = self.W / np.sqrt(np.sum(self.W**2, axis=1, keepdims=True))
+            self.orig_X = X
 
-    def __call__(self, x, learning_rate=0.0, lambd=0.01):
-        y = np.matmul(self.W, x)
-        if learning_rate > 0.0:
-            winner = np.argmax(y)
-            distance = np.sqrt(
-                np.sum(np.power(self.W - self.W[winner], 2), axis=1))
-            gaussian_neighbourhood = np.exp(-(distance * distance) /
-                                            (2 * lambd * lambd))
-            self.W = self.W + learning_rate * np.reshape(np.repeat(gaussian_neighbourhood, len(x)), (-1, len(x))) * (x - self.W)
+    def training_step(self, learning_rate):
+        winners = set()
+        W = self.W.copy()
+        for x in self.X:
+            # find winner
+            if self.normalize:
+                winner = np.argmax(np.matmul(W, x))
+            else:
+                winner = np.argmin(np.sum((W - x)**2, axis=1))
+
+            # update winner
+            W[winner] += learning_rate * (x - W[winner])
+            winners.add(winner)
+
+        # reset total loosers (dead neurons)
+        loosers = np.array(list(set(range(len(W))) - winners), dtype=np.int32)
+        W[loosers] = self.random_generator.uniform(np.min(self.X),
+                                                   np.max(self.X),
+                                                   size=(len(loosers),
+                                                         self.n_inputs))
+        if self.normalize:
+            W = W / np.sqrt(np.sum(W**2, axis=1, keepdims=True))
+
+        self._max_winner_step = np.max(self.W - W)
+        self._n_loosers = len(loosers)
+        self.W = W
+
+    def should_stop(self):
+        return self._n_loosers == 0 and self._max_winner_step < 0.0001 * (
+            np.max(self.X) - np.min(self.X))
 
 
-from sklearn.datasets import make_moons
-import matplotlib
-import matplotlib.pyplot as plt
+if __name__ == "__main__":
+    # simple Kohonen visualization
+    from sklearn.datasets import make_moons
+    import matplotlib
+    import matplotlib.pyplot as plt
 
-X, y = make_moons(n_samples=200, noise=0.05)
-kohonen = KohonenNetwork(2, 100)
+    X, y = make_moons(n_samples=200, noise=0.1)
+    kohonen = KohonenNetwork(15, X, normalize=False)
 
-fig, ax = plt.subplots()
-moons, = ax.plot(X[:, 0], X[:, 1], 'b.')
-neurons, = ax.plot(kohonen.W[:, 0], kohonen.W[:, 1], 'ro')
+    fig, ax = plt.subplots()
+    moons, = ax.plot(kohonen.X[:, 0], kohonen.X[:, 1], 'b.')
+    neurons, = ax.plot(kohonen.W[:, 0], kohonen.W[:, 1], 'ro')
 
+    def update(frame):
+        kohonen.training_step(learning_rate=0.1)
+        if kohonen.should_stop():
+            neurons.set_color('black')
+            anim.pause()
+        neurons.set_data(kohonen.W[:, 0], kohonen.W[:, 1])
+        return moons, neurons
 
-def update(frame):
-    for x in X:
-        kohonen(x, learning_rate=0.01)
-    neurons.set_data(kohonen.W[:, 0], kohonen.W[:, 1])
-    return moons, neurons
-
-
-anim = matplotlib.animation.FuncAnimation(fig, update, frames=range(1000), interval=100)
-plt.show()
+    anim = matplotlib.animation.FuncAnimation(fig,
+                                              update,
+                                              frames=range(1000),
+                                              interval=100)
+    plt.show()
