eda0d42a392d6d48349c2a671a0a73c6e8d3b292
Jan Karwowski <karwojan@tlen.pl>
2021-11-13 13:02:38 +0100

    Add some options to argparse

--- Task5/compression.py
+++ Task5/compression.py
@@ -15,16 +15,18 @@ def psnr(X, Y):
 
 
 def compress_image(filename, number_of_neurons, crop_size, number_of_crops,
-                   learning_rate, normalize):
+                   learning_rate, normalize, plot, output):
     # read image
     image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
 
     # extract random crops
     random_crops = np.array([
-        np.reshape(image[x:x + crop_size, y:y + crop_size], (-1,))
+        np.reshape(image[x:x + crop_size, y:y + crop_size], (-1, ))
         for x, y in zip(
-            np.random.randint(0, image.shape[0] - crop_size, size=(number_of_crops,)),
-            np.random.randint(0, image.shape[1] - crop_size, size=(number_of_crops,)))
+            np.random.randint(
+                0, image.shape[0] - crop_size, size=(number_of_crops, )),
+            np.random.randint(
+                0, image.shape[1] - crop_size, size=(number_of_crops, )))
     ])
 
     # clusterize random crops using kohonen network
@@ -49,9 +51,10 @@ def compress_image(filename, number_of_neurons, crop_size, number_of_crops,
             winner = kohonen.winner(np.expand_dims(crop, axis=0))
             decoded_image[i:i + crop_size, j:j + crop_size] = np.reshape(
                 kohonen.W[winner], (crop_size, crop_size)) * factor
-    cv2.imwrite("output.png", decoded_image)
-    plt.imshow(decoded_image, cmap='gray')
-    plt.show()
+    cv2.imwrite(output if output else "output.png", decoded_image)
+    if plot:
+        plt.imshow(decoded_image, cmap='gray')
+        plt.show()
 
     # calculate compression ratio
     n_image_pixels = image.shape[0] * image.shape[1]
@@ -74,7 +77,10 @@ if __name__ == '__main__':
     parser.add_argument("--number-of-crops", type=int, required=True)
     parser.add_argument("--learning-rate", type=float, required=True)
     parser.add_argument("--normalize", action="store_true")
+    parser.add_argument("--plot", action="store_true")
+    parser.add_argument("--output", type=str, required=False)
     args = parser.parse_args()
 
     compress_image(args.filename, args.number_of_neurons, args.crop_size,
-                   args.number_of_crops, args.learning_rate, args.normalize)
+                   args.number_of_crops, args.learning_rate, args.normalize,
+                   args.plot, args.output)
